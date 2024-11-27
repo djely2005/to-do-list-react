a27787bedae36be3e63d48cc7161816fc0ba3118
djely2005 <elyass162005@gmail.com>
2024-11-27 00:03:23 +0000

    Adding CheckBox

--- src/com/list.jsx
+++ src/com/list.jsx
@@ -7,17 +7,33 @@ function List({list, setItem, setEdit, setList}){
         })
     }
 
-    function editItem(key){
+    function editItemChecked(key){
+        setList((prev)=>{
+            let array = []
+            prev.forEach(element => {
+                if(element.key == key){
+                    element.checked = !element.checked;
+                    console.log(element)             
+                }
+                array.push(element);
+            });
+            return array;
+        })
+
+    }
+    function editItemContent(key){
         const TARGET = list.filter(element=>{return element.key == key})[0];
         setItem(TARGET.content);
         setEdit({bool: true, key: key})
     }
+
     let htmlElement = list.map((element)=>{
         return (
           <div className='flex justify-between w-full' key={element.key}>
+            <input type="checkbox" name="checker" defaultChecked={element.checked} onChange={()=>{editItemChecked(element.key)}}/>
             <li className='first-letter:uppercase'>{element.content}</li>
             <div>
-                <button className='bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded text-xl' onClick={()=>{editItem(element.key)}}>Modify</button>
+                <button className='bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded text-xl' onClick={()=>{editItemContent(element.key)}}>Modify</button>
                 <button className='bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded text-xl' onClick={()=>{removeItem(element.key)}}>Delete</button>
             </div>
           </div>
